8d7abca5ac633ef4ae88c7ad2523e284ea96aff4
Abdul <ahschohan06@gmail.com>
2025-02-26 18:08:12 +0100

    updated to recent

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Conectar a MongoDB
-mongoose.connect('mongodb+srv://Abdul:abdul1234@cluster1.8tcez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {
+mongoose.connect('mongodb+srv://Abdul:abdul1234@cluster1.8tcez.mongodb.net/LlibCat?retryWrites=true&w=majority&appName=Cluster1', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -17,34 +17,8 @@ mongoose.connect('mongodb+srv://Abdul:abdul1234@cluster1.8tcez.mongodb.net/?retr
   .catch(err => console.log('Error connecting to MongoDB:', err));
 
 // Definir rutas usando las funciones importadas
-app.get('/', (req, res) => {
-res.send('Hello World!');
-}
-);
-
-const LlibresSchema = new mongoose.Schema({
-  titol: String,
-  autor: String,
-  anyPublicacio: Number,
-  descripcio: String,
-  categories: [String]
-});
-
-
-const Llibres = mongoose.model('Llibreszz', LlibresSchema, 'Llibres');
-
 app.post('/add', saveLlibres);
-app.get('/list', (req, res) => {
-  try {
-    console.log('Fetching books');
-    const books = Llibres.find();
-    console.log(books);
-    res.status(200).json(books);
-  } catch (err) {
-    res.status(500).json({ message: 'Error fetching books', error: err.message });
-  }
-}
-);
+app.get('/list', getLlibres);
 app.get('/list/:dataini/:datafi', getByDate);
 
 // Iniciar servidor
