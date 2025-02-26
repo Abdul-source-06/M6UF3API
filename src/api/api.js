9292a0530cde241a5ba5a07526792ede34717664
Abdul <ahschohan06@gmail.com>
2025-02-26 18:13:17 +0100

    small change

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ const Llibres = require('../model/llibresModel');
 // Obtener todos los libros
 async function getLlibres(req, res) {
   try {
-    const books = await Llibres.find();
+    const books = await Llibres.find().limit(2);
     res.status(200).json(books);
   } catch (err) {
     res.status(500).json({ message: 'Error fetching books', error: err.message });
